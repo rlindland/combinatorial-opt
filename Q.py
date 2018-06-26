3164e5d0429f971d873de2f6976e749242331dd2
rlindland <robert.lindland@gmail.com>
2018-06-26 12:03:24 +0200

    'Correct' code

--- Q.py
+++ Q.py
@@ -7,6 +7,8 @@ import numpy as np
 class Q(nn.Module):
 
 	def __init__(self, dim, T):
+		super(Q, self).__init__()
+
 		self.T = T
 		self.dim = dim
 
@@ -20,24 +22,24 @@ class Q(nn.Module):
 
 	def embed(self,S,graph):
 		graph = torch.from_numpy(graph)
-		self.u = torch.zeros(self.dim,graph.shape[0])
+		self.u = torch.zeros(graph.shape[0],self.dim)
+		x = torch.tensor([1.0 if v in S else 0.0 for v in range(graph.shape[0])])
 		for _ in range(self.T):
 			u_prime=[]
-			summ = u@(torch.ones(u.shape[0])).view(u.shape[0],1)
+			summ = torch.ones(1,self.u.shape[0])@self.u
 			for v in range(graph.shape[0]):
-				theta1xv=self.theta1(x[v])
-				theta2sum=self.theta2(summ-u[:,v])
-				theta3sum=0 #self.theta3(F.relu(self.theta4(graph[v,:]@torch.ones(graph.shape[0])-graph[v,v])))
-				for v_prime in range(graph.shape[0]): theta3sum+=F.relu(self.theta4(graph(v,v_prime)))
+				theta1xv=self.theta1(x[v].view(1,1))
+				theta2sum=self.theta2(summ-self.u[v,:])
+				theta3sum=0 
+				for v_prime in range(graph.shape[0]): theta3sum+=F.relu(self.theta4(graph[v,v_prime].view(1,1)))#;print('3:', theta3sum.shape)
 				theta3sum=self.theta3(theta3sum)
 				u_prime.append(F.relu(theta1xv+theta2sum+theta3sum))
-			self.u=torch.t(torch.tensor(u_prime))
+			self.u=torch.cat(u_prime)
 
 	def q(self,v):
-		x = [1 if v in S else 0 for v in range(graph.shape[0])]
-		theta6sum=self.theta6(self.u@torch.ones(self.u.shape[0]))
-		theta7uv=self.theta7(self.u[:,v])
-		return float(self.theta5(F.relu(torch.cat(theta6sum,theta7uv))))
+		theta6sum=self.theta6(torch.ones(1,self.u.shape[0])@self.u)
+		theta7uv=self.theta7(self.u[v,:].view(1,-1))
+		return self.theta5(F.relu(torch.cat((theta6sum,theta7uv),dim=1)))
 
 	def forward(self,S,v,graph):
 		self.embed(S,graph)
--- main.py
+++ main.py
@@ -3,25 +3,29 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
-from q import Q
+from Q import Q
 
 DIM = 69
 E = 69
 T = 69 
-N = 69
+N = 1
 BATCH_SIZE = 69
-EPSILON = .69
-GAMMA=.69
+EPSILON = .2
+GAMMA=1
+
+torch.set_default_tensor_type(torch.DoubleTensor)
 
 Q = Q(DIM,T) 
 mse = nn.MSELoss(size_average=False)
-optimizer = optim.Adam(model.parameters())
+optimizer = optim.Adam(Q.parameters())
 
 def generate_graph():
-	n = np.random.randint(100,1000)
+	n = 5#np.random.randint(0,101)
 	matrix = np.random.randn(n,n)
 	matrix = matrix.T@matrix
 	np.fill_diagonal(matrix, 0)
+	return matrix
+
 
 def eps(eps): return (np.random.uniform()<eps)
 
@@ -29,9 +33,8 @@ for _ in range(E):
 	M,S,R=[],[],[]
 	graph = generate_graph()
 	S_bar = [i for i in range(graph.shape[0])]
-	v=0
 	cumloss=0
-	for t in range(T):
+	for t in range(min(T,graph.shape[0])): #change back
 		if eps(EPSILON):
 			idx = np.random.randint(len(S_bar))
 			v = S_bar[idx]
@@ -39,24 +42,25 @@ for _ in range(E):
 			with torch.no_grad():
 				Q.embed(S,graph)
 				max_val = Q.q(S_bar[0])
+				v=S_bar[0]
 				for node in S_bar: 
 					if Q.q(node)>max_val: v=node
 		S.append(v)
 		S_bar.remove(v)
-		if len(R)=0: R.append(0)
-		else: R.append(graph[S[t],v])
+		if len(R)==0: R.append(0)
+		else: R.append(-1*(graph[S[t],v]+graph[v,S[0]]))
 		if t>=N: 
 			Rsum=0
 			for i in range(t-N,t): Rsum+=R[i]
-			M.append((S[:t-n], S[t-n], Rsum, S[:t]))
+			M.append((S[:t-N], S[t-N], Rsum, S[:t]))
 			B = []
-			for _ in range(BATCH_SIZE):
+			for _ in range(min(BATCH_SIZE,t-N)): #change back
 				Q.zero_grad()
 				S_t_n, v_t_n, R_tn, S_t = M[np.random.randint(len(M))]
 				pred = Q(S_t_n,v_t_n,graph)
 				with torch.no_grad():
 					Q.embed(S_t,graph)
-					max_val = Q.q(S_bar[0])
+					max_val = Q.q(0)
 					for node in S_bar:
 						Qtn=Q.q(node) 
 						if Qtn>max_val: max_val=Qtn
@@ -65,7 +69,7 @@ for _ in range(E):
 				loss.backward()
 				optimizer.step()
 				cumloss += float(loss.data)
-	print('Epoch:', T+1,'/', EPOCH, '| Epoch Loss:', cumloss)
+	print('Epoch:', t+1,'/', E, '| Epoch Loss:', cumloss)
 
 
 
